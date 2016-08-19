717188e448fe8eee29bcd4d581b240ed0def7e34
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-08-19 03:56:18 -0400

    admin dash complete --pagination and details page next

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,14 +2,28 @@ app.controller('mainCtrl', function ($scope, $rootScope, $state,  $window, $loca
   $rootScope.$on('$stateChangeStart', function() { $window.scrollTo(0,0) });
   console.log('main!');
 });
+
 app.controller('homeCtrl', function ($scope, $state) {
   console.log('home!');
 });
 
-app.controller('admindashCtrl', function($scope, admindashService) {
+app.controller('admindashCtrl', function($scope, $state, admindashService) {
+  console.log('admin!');
+
     admindashService.getAllPrints().then(res => {
         $scope.prints = res.data;
-        console.log($scope.prints);
+        $scope.totalPrints = res.data.length;
+
+        $scope.totalDeadPercent = 0;
+        for( var i = 0; i < res.data.length; i++ ){
+            $scope.totalDeadPercent += parseInt( res.data[i].print_data.deadPercent, 10 );
+        }
+        $scope.deadPercentAvg = Math.ceil((($scope.totalDeadPercent)/(res.data.length)) * 100) /100;
+        $scope.totalLivePercent = 0;
+        for( var i = 0; i < res.data.length; i++ ){
+            $scope.totalLivePercent += parseInt( res.data[i].print_data.livePercent, 10 );
+        }
+        $scope.livePercentAvg = Math.ceil((($scope.totalLivePercent)/(res.data.length)) * 100) /100;
     });
 });
 
